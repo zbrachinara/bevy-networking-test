eaa6d2c62da82f939c8d2753cbd9064398c2ccf8
zbrachinara <zbrachinara@gmail.com>
2023-06-23 19:12:37 -0400

    add renet systems

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-use bevy::{prelude::App, DefaultPlugins};
+use bevy::{prelude::*, DefaultPlugins};
+use bevy_renet::{
+    renet::{RenetClient, RenetServer},
+    RenetClientPlugin, RenetServerPlugin,
+};
+
+enum Message {
+    Left,
+    Right,
+}
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+
+    let client_or_server = std::env::args()
+        .nth(1)
+        .expect("specify either server or client");
+    if client_or_server == "client" {
+        app.insert_resource(RenetClient::new(default()));
+        app.add_plugin(RenetClientPlugin);
+    } else if client_or_server == "server" {
+        app.insert_resource(RenetServer::new(default()));
+        app.add_plugin(RenetServerPlugin);
+    } else {
+        panic!("specify either server or client")
+    }
+
+    app.run();
 }
